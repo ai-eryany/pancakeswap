9593a45f4a29e9712919a178c1a477b1605aa0aa
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-10-23 22:56:17 +0300

    app.js vars.js functions.js created with the connectWallectModal

---
+++ scripts/functions.js
@@ -0,0 +1,20 @@
+const qs = (selector) => document.querySelector(selector);
+const qsa = (selector) => document.querySelectorAll(selector);
+
+const switcher = (htmlClass) => {
+    console.log(1);
+    htmlClass.addEventListener('click', (evt) => {
+        htmlClass.classList.toggle('modelBackground--flex');
+    });
+};
+
+const walletModalBlocker = (htmlClass) => {
+    htmlClass.addEventListener('click', (evt) => {
+        evt.stopPropagation();
+    });
+};
+
+const pairButtonEvent = (modelBackground, walletModal) => {
+    switcher(modelBackground);
+    walletModalBlocker(walletModal);
+};
